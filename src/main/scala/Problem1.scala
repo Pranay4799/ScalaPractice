fd52380fb716724dd7a67b546539dc2fab69f338
Pranay <pranayhalmare28@gmail.com>
2024-11-04 14:40:39 +0530

    Question1

--- src/main/scala/Problem1.scala
+++ src/main/scala/Problem1.scala
@@ -4,43 +4,43 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.{avg, col, count, current_date, current_timestamp, datediff, initcap, max, min, sum, to_date, when}
 import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
 
-
-
 object Problem1 {
   def main(args:Array[String]):Unit=
     {
       val spark_conf = new SparkConf()
-      spark_conf.set("spark.app.name","Question2")
+      spark_conf.set("spark.app.name","Problem")
       spark_conf.set("spark.master","local[*]")
-
       val spark = SparkSession.builder()
         .config(spark_conf)
         .getOrCreate()
 
       import spark.implicits._
 
-      val sales = List(
-        ("karthik", 60000),
-        ("neha", 48000),
-        ("priya", 30000),
-        ("mohan", 24000),
-        ("ajay", 52000),
-        ("vijay", 45000),
-        ("veer", 70000),
-        ("aatish", 23000),
-        ("animesh", 15000),
-        ("nishad", 8000),
-        ("varun", 29000),
-        ("aadil", 32000)
-         ).toDF("name","total_sales")
+      val employees = List(
+        ("karthik", "2024-11-01"),
+        ("neha", "2024-10-20"),
+        ("priya", "2024-10-28"),
+        ("mohan", "2024-11-02"),
+        ("ajay", "2024-09-15"),
+        ("vijay", "2024-10-30"),
+        ("veer", "2024-10-25"),
+        ("aatish", "2024-10-10"),
+        ("animesh", "2024-10-15"),
+        ("nishad", "2024-11-01"),
+        ("varun", "2024-10-05"),
+        ("aadil", "2024-11-04")
+      ).toDF("name", "last_checkin")
 
-      val performance_status = sales.select(initcap(col("name")),col("total_sales"),
-        when(col("total_sales")>50000,"Excellent")
-          .when(col("total_sales")>25000 && col("total_sales")<=50000,"Good")
-          .otherwise("Needs Improvement").alias("status"))
-      performance_status.show()
+      val status1 = employees.select(initcap(col("name")),
+        when(datediff(current_date(),to_date(col("last_checkin")))<=7,"Active")
+          .otherwise("Inactive").alias("Status"))
 
-      performance_status.groupBy("status").agg(sum(col("total_sales"))).show()
+      employees.createOrReplaceTempView("employees")
+
+      status1.show()
+      val status2 = spark.sql("select initcap(name),CASE WHEN datediff(current_date(),to_date(last_checkin))<=7 THEN 'Active' ELSE 'Inactive' END AS Status from employees")
+
+      status2.show()
 
     }
 }
